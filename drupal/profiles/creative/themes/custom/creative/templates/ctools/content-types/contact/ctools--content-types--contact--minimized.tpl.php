352c7ddb513dafa5f9c8e80615b24a917954dc2a
Vladimir <vladimir.gaydamaka@propeople.com.ua>
2015-06-17 19:37:02 +0300

    Create display

--- drupal/profiles/creative/themes/custom/creative/templates/ctools/content-types/contact/ctools--content-types--contact--minimized.tpl.php
+++ drupal/profiles/creative/themes/custom/creative/templates/ctools/content-types/contact/ctools--content-types--contact--minimized.tpl.php
@@ -7,17 +7,17 @@
  *   An array with all data from settings form.
  */
 ?>
-<section role="region" class="contact--panel">
+<section role="region" class="contact--minimized">
   <?php if (!empty($content['override_title_text'])): ?>
     <h4 role="heading">
       <?php print $content['override_title_text']; ?>
     </h4>
   <?php endif; ?>
-  <div class="about">
-    <address>
-      <?php print t('Adress'); ?>: <?php print $content['street']; ?> <?php print $content['build']; ?>, <?php print $content['zip']; ?> <?php print $content['city']; ?>
-      <br />
-      <?php print t('Telefon'); ?>: <?php print $content['phone']; ?>  E-post: <a href="mailto:<?php print $content['email']; ?>"><?php print $content['email']; ?></a>
-    </address>
-  </div>
+  <address>
+    <?php print $content['zip']; ?> <?php print $content['city']; ?>
+    |
+    <?php print $content['street']; ?>, <?php print $content['build']; ?>
+    |
+    <?php print $content['phone']; ?>
+  </address>
 </section>
