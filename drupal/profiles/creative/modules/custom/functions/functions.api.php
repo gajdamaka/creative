6927d279e7f10acb77374137825b095ab12e0b9a
Vladimir <vladimir.gaydamaka@propeople.com.ua>
2015-06-17 17:12:54 +0300

    Add helped module

---
+++ drupal/profiles/creative/modules/custom/functions/functions.api.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @file
+ * Module API.
+ */
+
+/**
+ * The machine name of a module. Defined by "autoload" module.
+ *
+ * @see autoload()
+ */
+define('FUNCTIONS_MODULE_NAME', '');
+/**
+ * Relative, to a Drupal installation, path.  Defined by "autoload" module.
+ *
+ * @see autoload()
+ */
+define('FUNCTIONS_MODULE_PATH', '');
