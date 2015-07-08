ddf97c27550dca24c340420135b26cc7847b581e
Vladimir <vladimir.gaydamaka@propeople.com.ua>
2015-07-08 18:38:33 +0300

    Slow down menu by mobile

--- drupal/profiles/creative/themes/custom/creative/js/script.js
+++ drupal/profiles/creative/themes/custom/creative/js/script.js
@@ -4,14 +4,21 @@
   Theme.has('html', function($body) {
     var $menu = $('.mobile-menu-wrapper'),
         $overlay = $('#overlay'),
-        $header = $('header');
+        $header = $('header .wrapper');
 
     $body.find('.mobile-menu').on('click', function() {
-      //$menu.toggleClass('open');
-      $overlay.css('top', $header.outerHeight() + $header.offset().top).toggle();
+      if ($menu.hasClass('open')){
+        $menu.stop().hide(500);
+        $menu.removeClass('open');
+      }
+      else {
+        $menu.toggleClass('open');
+        $menu.stop().show(500);
+      }
+
+      setTimeout(function() {
+        $overlay.css('top', $header.outerHeight()).toggle()
+      }, 500);
     });
-    $('.carousel').carousel({
-      interval: 5000 //changes the speed
-    })
   });
 })(jQuery);
